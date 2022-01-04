aa8e373445667a49e211524f349a16664db41eda
Artsiom Lipchyk <tommerg09@gmail.com>
2022-01-04 20:35:08 +0300

    add async examples

---
+++ 3_generators.py
@@ -0,0 +1,48 @@
+from time import time
+
+
+def gen(s):
+    for i in s:
+        yield i
+
+g = gen('oleg')
+
+
+def gen_filename():
+    while True:
+        pattern = 'file-{}.jpeg'
+        t = int(time() * 1000)
+        yield pattern.format(str(t))
+
+        sum = 2 + 3
+        print(sum)
+
+
+gn = gen_filename()
+
+
+def gen1(s):
+    for i in s:
+        yield i
+
+
+def gen2(n):
+    for i in range(n):
+        yield i
+
+
+g1 = gen1('oleg')
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+while tasks:
+    task = tasks.pop(0)
+
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)
+    except StopIteration:
+        pass
+      
